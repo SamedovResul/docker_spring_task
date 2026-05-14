2edbe8f030e9179fcee7e7323e0801e977c1c503
SamedovResul <samedovrasul7@gmail.com>
2026-05-14 17:00:11 +0400

    seperate apiController.java form SpringBootDockerApplication.java file

--- src/main/java/com/example/spring_boot_docker/SpringBootDockerApplication.java
+++ src/main/java/com/example/spring_boot_docker/SpringBootDockerApplication.java
@@ -1,13 +1,17 @@
 package com.example.springbootdocker;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class ApiController {
+@SpringBootApplication
+public class SpringBootDockerApplication {
 
-    @GetMapping("/api/hello")
-    public String hello() {
-        return "Hello From Spring Boot API 🚀";
-    }
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDockerApplication.class, args);
+	}
+
 }
