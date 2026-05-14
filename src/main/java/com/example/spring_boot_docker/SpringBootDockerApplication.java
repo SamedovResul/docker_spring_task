9454119a922a55d41f98f982ba9b05aec05196b2
SamedovResul <samedovrasul7@gmail.com>
2026-05-14 17:04:43 +0400

    remove @RestController from SpringBootDockerApplication.java

--- src/main/java/com/example/spring_boot_docker/SpringBootDockerApplication.java
+++ src/main/java/com/example/spring_boot_docker/SpringBootDockerApplication.java
@@ -3,7 +3,7 @@ package com.example.springbootdocker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@RestController
+
 @SpringBootApplication
 public class SpringBootDockerApplication {
 
